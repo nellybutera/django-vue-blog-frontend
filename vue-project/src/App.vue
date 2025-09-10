fe975e87f7180cafa705b0ba5cde9a4b7750bbcc
nellybutera <buteranelly250@gmail.com>
2025-09-10 23:30:51 +0200

    refactor: simplify App.vue structure by removing unused components and enhancing navigation

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,47 +1,34 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div id="app" class="min-h-screen bg-gray-100 font-sans antialiased text-gray-800">
+    <!-- Navigation Bar -->
+    <nav class="bg-white shadow-lg py-4 px-6 md:px-12 flex justify-between items-center">
+      <div class="text-2xl font-bold text-blue-600">
+        <router-link to="/">Vue Blog</router-link>
+      </div>
+      <div class="space-x-4 md:space-x-6">
+        <router-link to="/" class="text-gray-600 hover:text-blue-600 transition-colors duration-200">Home</router-link>
+        <router-link to="/posts" class="text-gray-600 hover:text-blue-600 transition-colors duration-200">Posts</router-link>
+        <router-link to="/login" class="text-gray-600 hover:text-blue-600 transition-colors duration-200">Login</router-link>
+      </div>
+    </nav>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <!-- Main Content Area -->
+    <main class="container mx-auto mt-8 p-4 md:p-8">
+      <router-view></router-view>
+    </main>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style>
+/* Basic styling for the router-link active state */
+.router-link-active {
+  color: #3b82f6;
+  font-weight: 600;
 }
 </style>
